5a06a2de7a916c0d01b5ee5675460c4b8a76fa99
Ashu81045 <60062103+Ashu81045@users.noreply.github.com>
2026-08-27 18:24:39 +0530

    final fixes

--- src/lib/share/generateBillPdf.ts
+++ src/lib/share/generateBillPdf.ts
@@ -1,5 +1,5 @@
 import { jsPDF } from 'jspdf'
-import { drawA4Bill } from '../pdf/DrawA4bill'
+import { drawA4Bill } from '../pdf/drawA4Bill'
 import { buildBillLabels } from '../billing/billLabels'
 import { getCachedBusinessSettings } from '../../hooks/useBusinessSettings'
 import type { BillPreviewData } from '../../features/billing/components/BillPreview'
